a7d61fb374e7cbde04b3836341f6cebdee99462b
Wojciech Król <wk20981@gmail.com>
2024-04-11 09:49:28 +0200

    refactor: Improve the execution of actions and enhance token counting in the history result view.

--- src/views/HistoryView.tsx
+++ src/views/HistoryView.tsx
@@ -1,6 +1,7 @@
 import { Action, ActionPanel, Color, Detail, Icon } from "@raycast/api";
-import { GPTTokens } from "gpt-tokens";
-import { useMemo, useState } from "react";
+import { ChatCompletionMessageParam } from "openai/resources";
+import React, { useMemo, useState } from "react";
+import { useMetadata } from "../hooks";
 import { getModelName } from "../lib/OpenAI";
 import { useHistoryState } from "../store/history";
 
@@ -8,52 +9,32 @@ interface Props {
   id: string;
 }
 
-interface ResultMetadata {
-  promptTokens: number;
-  resultTokens: number;
-  totalTokens: number;
-  cost: number;
-}
-
 export default function HistoryView({ id }: Props) {
   const history = useHistoryState((state) => state.history.find((a) => a.id === id));
   const [displayPrompt, setDisplayPrompt] = useState<boolean>(false);
 
-  const metadata = useMemo<ResultMetadata>(() => {
-    if (history === undefined) {
-      return {
-        promptTokens: 0,
-        resultTokens: 0,
-        totalTokens: 0,
-        cost: 0,
-      };
+  const chat = useMemo<ChatCompletionMessageParam[]>(() => {
+    if (!history) {
+      return [];
     }
 
-    const usage = new GPTTokens({
-      model: history.action.model as GPTTokens["model"],
-      messages: [
-        {
-          role: "system",
-          content: history.action.systemPrompt,
-        },
-        {
-          role: "user",
-          content: history.prompt,
-        },
-        {
-          role: "assistant",
-          content: history.result,
-        },
-      ],
-    });
-
-    return {
-      promptTokens: usage.promptUsedTokens,
-      resultTokens: usage.completionUsedTokens,
-      totalTokens: usage.usedTokens,
-      cost: usage.usedUSD,
-    };
+    return [
+      {
+        role: "system",
+        content: history.action.systemPrompt,
+      },
+      {
+        role: "user",
+        content: history.prompt,
+      },
+      {
+        role: "assistant",
+        content: history.result,
+      },
+    ];
   }, [history]);
+
+  const metadata = useMetadata(chat, history?.action.model || "gpt-3.5-turbo");
 
   if (!history) {
     return (
@@ -78,10 +59,12 @@ export default function HistoryView({ id }: Props) {
           <Detail.Metadata.TagList title="Model">
             <Detail.Metadata.TagList.Item text={getModelName(history.action.model)} color={Color.SecondaryText} />
           </Detail.Metadata.TagList>
-          <Detail.Metadata.Label title="Prompt Tokens" text={metadata.promptTokens.toString()} />
-          <Detail.Metadata.Label title="Result Tokens" text={metadata.resultTokens.toString()} />
-          <Detail.Metadata.Label title="Total Tokens" text={metadata.totalTokens.toString()} />
-          <Detail.Metadata.Label title="Cost" text={`$${metadata.cost.toFixed(6)}`} />
+          <React.Fragment key={JSON.stringify(metadata)}>
+            <Detail.Metadata.Label title="Prompt Tokens" text={metadata.promptTokens.toString()} />
+            <Detail.Metadata.Label title="Result Tokens" text={metadata.resultTokens.toString()} />
+            <Detail.Metadata.Label title="Total Tokens" text={metadata.totalTokens.toString()} />
+            <Detail.Metadata.Label title="Cost" text={`$${metadata.cost.toFixed(6)}`} />
+          </React.Fragment>
         </Detail.Metadata>
       }
       actions={
